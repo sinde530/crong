497be597d7f5f4947ecdd267f390a4d886b32356
kimsungeun <mirxx@naver.com>
2021-08-30 13:54:15 +0900

    Link Modify

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import '../Styles/header.css'
+import { Route, Link } from 'react-router-dom';
+import Home from '../Pages/Home';
+import Content from '../Pages/Content';
 
 function Header() {
+
     return (
         <div className="header">
             <div className="header-box">
-            <h1>Blog Class Naming</h1>
+                <h1>Blog Class Naming</h1>
+                <div className="openMenu"><i class="fas fa-bars"></i></div>
                 <ul className="header-list">
-                    <li className="header-item">My Home</li>
-                    <li className="header-item">Content</li>
-                    <button className="toggle-btn">카테고리 Click
-                    <div class="navicon"></div>
-                    </button>
+                    <li className="header-item">
+                        <Link to="/">My Home</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link to="/Content">Content</Link>
+                    </li>
+                    <Route path="/" exact = {true} component={Home} />
+                    <Route path="/Content" component={Content} />
+                    <div className="closeMenu"><i className="fa fa-times"></i></div>
+                    <span className="icons">
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-github"></i>
+                    </span>
                 </ul>
             </div>
         </div>
-        
+
     );
 }
 
---
+++ src/Components/Main.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+function Main() {
+    var currentDate = ("08/26/2021");
+    var DevelopBlog = ("DEVELOPMENT BLOG");
+    let [Title, TitleChange] = useState([
+        // 1
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.',
+        // 2
+        'Component2',
+        // 3
+        'Component3']);
+    let [Good, GoodChange] = useState(0);
+
+
+    return (
+
+
+        <div className="listall">
+
+            <div className="list-box">
+                <div className="title">
+                    <p className="title-data">{currentDate}</p>
+                    <p className="title-text">{DevelopBlog}</p>
+                </div>
+                <h1 className="title-hd">Project1</h1>
+                <h2 className="title-content">{Title[0]}</h2>
+                <span className="good-btn" onClick={() => { GoodChange(Good + 1) }}> 💜 </span> {Good}
+            </div>
+
+            <div className="list-box">
+                <div className="title">
+                    <p className="title-data">{currentDate}</p>
+                    <p className="title-text">{DevelopBlog}</p>
+                </div>
+                <h1 className="title-hd">Project2</h1>
+                <h2 className="title-content">{Title[0]}</h2>
+                <span className="good-btn" onClick={() => { GoodChange(Good + 1) }}> 💜 </span> {Good}
+            </div>
+
+            <div className="list-box">
+                <div className="title">
+                    <p className="title-data">{currentDate}</p>
+                    <p className="title-text">{DevelopBlog}</p>
+                </div>
+                <h1 className="title-hd">Project3</h1>
+                <h2 className="title-content">{Title[0]}</h2>
+                <span className="good-btn" onClick={() => { GoodChange(Good + 1) }}> 💜 </span> {Good}
+            </div>
+
+            <div className="list-box">
+                <div className="title">
+                    <p className="title-data">{currentDate}</p>
+                    <p className="title-text">{DevelopBlog}</p>
+                </div>
+                <h1 className="title-hd">Project4</h1>
+                <h2 className="title-content">{Title[0]}</h2>
+                <span className="good-btn" onClick={() => { GoodChange(Good + 1) }}> 💜 </span> {Good}
+            </div>
+
+            <div className="list-box">
+                <div className="title">
+                    <p className="title-data">{currentDate}</p>
+                    <p className="title-text">{DevelopBlog}</p>
+                </div>
+                <h1 className="title-hd">Project5</h1>
+                <h2 className="title-content">{Title[0]}</h2>
+                <span className="good-btn" onClick={() => { GoodChange(Good + 1) }}> 💜 </span> {Good}
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Main;
