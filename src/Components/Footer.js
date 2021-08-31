29b6159acc1f39472993d3c662661128dac47cfe
kimsungeun <mirxx@naver.com>
2021-08-31 10:36:36 +0900

    change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,8 +10,8 @@ function Footer() {
         <div className="footer">
             <span className="icons">
                 <a href="https://github.com/"><i className="fa-github"><FaGithub/></i></a>
-                <a href="https://www.instagram.com/"><i className="fa-instagram"><FaTwitterSquare/></i></a>
-                <a href="https://twitter.com/?lang=ko"><i className="fa-twitter"><FaInstagram/></i></a>
+                <a href="https://twitter.com/?lang=ko"><i className="fa-twitter"><FaTwitterSquare/></i></a>
+                <a href="https://www.instagram.com/"><i className="fa-instagram"><FaInstagram/></i></a>
                 <a href="https://www.facebook.com/"><i className="fa-facebook"><FaFacebookF/></i></a>
             </span>
         </div>
