8625cb935111c9a9fe5940a5b62c63087c0d6923
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-09 20:44:28 +0900

    migration -2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Footer from './Components/Footer';
 import Header from './Components/Header';
